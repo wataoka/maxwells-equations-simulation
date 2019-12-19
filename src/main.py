fef45bc583648eafc181160433f030f77dc37eb3
wataoka <wataoka@stu.kobe-u.ac.jp>
2019-12-19 16:49:51 +0900

    add: plt

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import copy
 import math
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 def gaussian_pulse(t, A=1.0, tau=4.0):
     alpha = (4/tau)**2
@@ -9,8 +10,19 @@ def gaussian_pulse(t, A=1.0, tau=4.0):
     return pulse
 
 def show_result(arr):
-    for a in arr:
-        print(*list(map(lambda x: '{0:>8.2f}'.format(x), a)))
+    print(*list(map(lambda x: '{0:>8.2f}'.format(x), arr)))
+
+def update_graph(frame, x, y):
+    pyplot.cla()
+    x.append(frame)
+    y.append(math.sin(frame))
+
+def draw_plot(E, B):
+    lines.set_data(X, E)
+    ax.set_xlim((X.min(), X.max()))
+    plt.pause(0.01)
+
+
 
 if __name__ == "__main__":
 
@@ -26,6 +38,10 @@ if __name__ == "__main__":
     B = [0 for _ in range(num_space)]
 
     refrection = False
+
+    fig, ax = plt.subplots(1, 1)
+    X = np.arange(-10, 10, 0.1)
+    lines, = ax.plot(X, E)
 
     # simulation
     for t in range(num_time-1):
@@ -52,4 +68,4 @@ if __name__ == "__main__":
         for x in range(num_space-1):
             B[x] = pre_B[x] + (dt/dx)*(E[x+1]-E[x])
         
-        print(*list(map(lambda x: '{0:>8.2f}'.format(x), E)))
+        draw_plot(E, B, lines)
