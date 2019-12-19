7655d42d8a58fc0d6d42e6fb713601929638b2bd
wataoka <wataoka@stu.kobe-u.ac.jp>
2019-12-19 10:34:26 +0900

    add: show result

--- src/fdtd_1d.py
+++ src/fdtd_1d.py
@@ -1,9 +1,8 @@
 import math
 
-def gaussian_pulse(E, A=1.0, tau=1.0, alpha=(4/1.0)**2):
+def gaussian_pulse(idx, E, A=1.0, tau=1.0, alpha=(4/1.0)**2):
     pulse = A * (math.e ** (-alpha*(tau**2)))
-    for i in range(len(E)): 
-        E[0][i] = pulse
+    E[0][idx] = pulse
     return E
 
 if __name__ == "__main__":
@@ -22,14 +21,14 @@ if __name__ == "__main__":
         # update E
         for x in range(num_space):
             if t == 0:
-                E = gaussian_pulse(E, A=10**7)
+                E = gaussian_pulse(idx=len(E)//2, E=E, A=10**7)
                 continue
             if x == 0:
-                E[t+1][x] = ((c*dt-dx)/(c*dt+dx))*(E[t+1][x+1] - E[t][x])
+                E[t][x] = ((c*dt-dx)/(c*dt+dx))*(E[t][x+1] - E[t-1][x])
             elif x == num_space-1:
-                E[t+1][x] = ((c*dt-dx)/(c*dt+dx))*(E[t+1][x-1] - E[t][x])
+                E[t][x] = ((c*dt-dx)/(c*dt+dx))*(E[t][x-1] - E[t-1][x])
             else:
-                E[t+1][x+1] = ((dx)/(dt))*(B[t+1][x] - B[t][x])*E[t+1][x]
+                E[t][x+1] = ((dx)/(dt))*(B[t][x] - B[t-1][x])*E[t-1][x]
             
         # update B
         for x in range(num_space-1):
@@ -39,6 +38,7 @@ if __name__ == "__main__":
                 B[t][x] = ((dt)/(dx))*(E[t][x+1] - E[t][x]) + B[t-1][x]
 
 for e in E:
-    print(e)
+    print(*list(map(lambda x: '{:.3f}\t'.format(x), e)))
+print()
 for b in B:
-    print(b)
+    print(*list(map(lambda x: '{:.3f}\t'.format(x), b)))
