1f23927f1af0ec5a89cd4e07768c3bbc27f49aa3
wataoka <wataoka@stu.kobe-u.ac.jp>
2019-12-19 17:34:21 +0900

    basic imple on gaussian pulse

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 
-def gaussian_pulse(t, A=1.0, tau=4.0):
+def gaussian_pulse(t, A=1.0, tau=20):
     alpha = (4/tau)**2
     pulse = A * np.exp(-alpha*(t - tau)**2)
     return pulse
@@ -12,16 +12,12 @@ def gaussian_pulse(t, A=1.0, tau=4.0):
 def show_result(arr):
     print(*list(map(lambda x: '{0:>8.2f}'.format(x), arr)))
 
-def update_graph(frame, x, y):
-    pyplot.cla()
-    x.append(frame)
-    y.append(math.sin(frame))
-
-def draw_plot(E, B):
-    lines.set_data(X, E)
+def draw_plot(E, B, lines, ax, ylim=1):
+    lines.set_data(X, B)
     ax.set_xlim((X.min(), X.max()))
-    plt.pause(0.01)
-
+    ax.set_ylim((-ylim, ylim))
+    plt.draw()
+    plt.pause(0.1)
 
 
 if __name__ == "__main__":
@@ -31,8 +27,8 @@ if __name__ == "__main__":
     dt = 0.5
     mu = 1.0
     delta = 1.0
-    num_time = 20
-    num_space = 20
+    num_time = 20000
+    num_space = 100
 
     E = [0 for _ in range(num_space)]
     B = [0 for _ in range(num_space)]
@@ -40,7 +36,7 @@ if __name__ == "__main__":
     refrection = False
 
     fig, ax = plt.subplots(1, 1)
-    X = np.arange(-10, 10, 0.1)
+    X = np.arange(-(num_space//2), (num_space//2), dx)
     lines, = ax.plot(X, E)
 
     # simulation
@@ -52,20 +48,20 @@ if __name__ == "__main__":
         for x in range(num_space):
             if t == 0:
                 continue
-            if x == 0 and not(refrection):
-                continue
-            elif x == num_space-1 and not(refrection):
-                continue
+            if (x==0 or x==1) and not(refrection):
+                E[x] = 0
+            elif (x==num_space-1 or x==num_space-2) and not(refrection):
+                E[x] = 0
             if x == 0 and refrection:
                 E[x] = ((c*dt-dx)/(c*dt+dx))*(E[x+1] - pre_E[x])
             elif x == num_space-1 and refrection:
                 E[x] = ((c*dt-dx)/(c*dt+dx))*(E[x] - pre_E[x-1])
             else:
                 E[x] = pre_E[x] + pow(c, 2)*(dx/dt)*(pre_B[x] - pre_B[x-1])
-        E[num_space//2] += gaussian_pulse(t=t)
+        E[num_space//2] += gaussian_pulse(t)
             
         # update B
         for x in range(num_space-1):
             B[x] = pre_B[x] + (dt/dx)*(E[x+1]-E[x])
         
-        draw_plot(E, B, lines)
+        draw_plot(E, B, lines, ax)
